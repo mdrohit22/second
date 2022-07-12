e87182222cc8cab9a00642e374c2e880fa5ab846
mdrohit22 <108532993+mdrohit22@users.noreply.github.com>
2022-07-12 07:54:56 +0530

    Add files via upload

---
+++ exception_handling.py
@@ -0,0 +1,15 @@
+print("a number")
+num1 = input()
+print("another NUMBER")
+num2 = input()
+
+
+try:
+    
+    
+    print("The sum of these two number is" ,
+      int(num1) + int(num2))
+except Exception as f:
+    print(f)
+
+print("This is very  important ")
